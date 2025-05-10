dc836cdc14f8293ada8a9b536c1a8e5cc6a8455d
Vorobyev Vyacheslav Romanovich <vyachvorobyev@ozon.ru>
2025-05-10 17:47:03 +0300

    Refactor HomeView.vue by removing unused imports and the disabled state variable to streamline the component.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-import { Button } from 'primevue'
 import { RouterView } from 'vue-router'
-import { ref } from 'vue'
 import Header from '@/components/header/Header.vue'
-const disabled = ref(true)
 </script>
 
 <template>
