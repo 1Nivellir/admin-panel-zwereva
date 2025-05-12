ad7962524485c4999db2048c22c79ca9a167c4f3
Vorobyev Vyacheslav Romanovich <vyachvorobyev@ozon.ru>
2025-05-12 21:15:31 +0300

    Update API_TOKEN in useCustomFetch.ts to a new value for enhanced security and access control.

--- src/composables/useCustomFetch.ts
+++ src/composables/useCustomFetch.ts
@@ -4,7 +4,7 @@ import type { AxiosRequestConfig } from 'axios';
 import { getCookie } from '@/utils/cookies'
 import router from '@/router';
 
-export const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQURNSU4iLCJpZCI6MSwibG9naW4iOiJseHdxVlFNN3hEIiwic3ViIjoibHh3cVZRTTd4RCIsImlhdCI6MTc0Njg2Njk4MSwiZXhwIjoxNzQ3MDEwOTgxfQ.0SPLGZjgqnTiaY2vtXMkgnIyXBxs07IimTo21TXPAa8'
+export const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQURNSU4iLCJpZCI6MSwibG9naW4iOiJseHdxVlFNN3hEIiwic3ViIjoibHh3cVZRTTd4RCIsImlhdCI6MTc0NzA3MzYxMCwiZXhwIjoxNzQ3MjE3NjEwfQ.9BHKARZSD49JnqJ1yZHv5LzVa6vEbS62HyUq9LKXM10'
 const BASE_URL = 'https://zvereva-law.ru/';
 
 
