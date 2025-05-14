ad8f69b51b577be22d9c02dfceee1080841a0f28
Vorobyev Vyacheslav Romanovich <vyachvorobyev@ozon.ru>
2025-05-14 13:04:12 +0300

    Update root store to use current route for navigation after fetching visits
    - Modified the root store to import and utilize `useRoute` from `vue-router`.
    - Changed the navigation logic to redirect to the current route path instead of a hardcoded path after fetching visit data, improving flexibility in routing.

--- src/stores/root.ts
+++ src/stores/root.ts
@@ -2,15 +2,16 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { useCustomFetch } from '@/composables/useCustomFetch'
 import { API_VISITS } from '@/utils/apiPath'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 export const useRootStore = defineStore('counter', () => {
   const visits = ref(0)
   const router = useRouter()
+  const route = useRoute()
   const fetchVisits = async () => {
     try {
       const response = await useCustomFetch(API_VISITS)
       if (response.data) {
-        router.push('/applications')
+        router.push(route.path)
       }
       visits.value = response.data
     } catch (error) {
